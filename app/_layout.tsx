1d4bcdb424563a7edf99a79ca4db15f075c9a910
Michael Aguilera <michaelaguilera12345@gmail.com>
2026-01-27 13:01:20 +0000

    feat: wrap app in ThemeProvider

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,21 @@
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
+import { ThemeProvider, useTheme } from '@/lib/ThemeContext';
+
+function RootLayoutInner() {
+  const { isDark } = useTheme();
+  return (
+    <>
+      <StatusBar style={isDark ? 'light' : 'dark'} />
+      <Stack screenOptions={{ headerShown: false }} />
+    </>
+  );
+}
 
 export default function RootLayout() {
   return (
-    <>
-      <StatusBar style="light" />
-      <Stack screenOptions={{ headerShown: false }} />
-    </>
+    <ThemeProvider>
+      <RootLayoutInner />
+    </ThemeProvider>
   );
 }
