f33835699357517c725192b01186a026ca6c7a48
Michael Aguilera <michaelaguilera12345@gmail.com>
2026-01-29 12:02:14 +0000

    feat(components): add dungeon component barrel export

---
+++ components/dungeon/index.ts
@@ -0,0 +1,6 @@
+// components/dungeon/index.ts
+export { PixelSprite } from './PixelSprite';
+export { DungeonCard } from './DungeonCard';
+export { DungeonBar } from './DungeonBar';
+export { ConsumableIcon } from './ConsumableIcon';
+export { ChestReveal } from './ChestReveal';
